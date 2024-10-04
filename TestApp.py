b2286e32ffbde665df540e598b86bc88d7f4b913
D-Satoh-01 <ak74g3@gmail.com>
2024-10-04 14:48:18 +0900

    Add files via upload

--- TestApp.py
+++ TestApp.py
@@ -3,6 +3,9 @@ import streamlit as st
 
 st.title('ヒット&ブロー（数当てゲーム）')
 
+count_hit = 0
+count_blow = 0
+
 def ButtonClick():
     number_inputted = editbox_1
 
@@ -49,5 +52,5 @@ editbox_1 = st.text_input('4桁の数字を入力してください', key='eb_1'
 button_1 = st.button('チェック', on_click=ButtonClick)
 
 
-show_count_hit = st.text(count_hit)
-show_count_blow = st.text(count_blow)
+show_count_hit = st.text('ヒット（数値と位置の両方が解答と合致している数）：'), st.text(count_hit)
+show_count_blow = st.text('ブロー（位置は異なるが解答の数値を含んでいる数）：'), st.text(count_blow)
